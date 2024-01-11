1bd09c0686743d5ec33a1608194e9a38086c3c66
huydq <wepiohip@gmail.com>
2024-01-11 15:57:54 +0700

    up

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-print("Huy")
+# python -u app.py
+import datetime
+
+# 
+print("Print")
+# 
+variable = 10
+print(variable)
+# 
+variable = variable +12
+print(variable)
+
+# 
+abcd = 'abcdef'
+print(abcd[1:3])
+
+# 
+str2= 2222
+str = f'Huy đẹp trai {str2}'
+print(str)
+
+
+# 
+yearx = input("Năm sinh: ")
+today = datetime.date.today()
+year = today.year
+age = year - int(yearx)
+print(age)
+
